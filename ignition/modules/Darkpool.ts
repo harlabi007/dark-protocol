8937ba5852799d7d80aef8e3f3f5f5828f6897e9
Alabi Idowu <alabiidowuolaoluwa@gmail.com>
2026-06-03 07:06:04 +0100

    cleanup hardhat files

--- ignition/modules/Darkpool.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const DarkPoolModule = buildModule("DarkPoolModule", (m) => {
-  const darkPool = m.contract("DarkPoolAuction");
-  return { darkPool };
-});
-
-export default DarkPoolModule;
